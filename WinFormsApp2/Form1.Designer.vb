67d17c7fa61434d0e242f801b4fec9dbdc5efcb8
Mariana M <mariengca27@gmail.com>
2021-04-17 20:31:59 -0300

    Projeto Windows Forms linguagem VB.NET

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnNome = New System.Windows.Forms.Button()
+        Me.btnSair = New System.Windows.Forms.Button()
+        Me.btnAdiciona = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnElse = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.btnComparar = New System.Windows.Forms.Button()
+        Me.btnVerifica = New System.Windows.Forms.Button()
+        Me.btnFor = New System.Windows.Forms.Button()
+        Me.lstDados = New System.Windows.Forms.ListBox()
+        Me.btnWhile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(27, 12)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(120, 15)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Coloque o seu nome."
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(12, 30)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(142, 23)
+        Me.txtName.TabIndex = 1
+        Me.txtName.Tag = ""
+        '
+        'btnNome
+        '
+        Me.btnNome.Location = New System.Drawing.Point(12, 246)
+        Me.btnNome.Name = "btnNome"
+        Me.btnNome.Size = New System.Drawing.Size(238, 69)
+        Me.btnNome.TabIndex = 2
+        Me.btnNome.Text = "&OK"
+        Me.btnNome.UseVisualStyleBackColor = True
+        '
+        'btnSair
+        '
+        Me.btnSair.Location = New System.Drawing.Point(521, 246)
+        Me.btnSair.Name = "btnSair"
+        Me.btnSair.Size = New System.Drawing.Size(238, 69)
+        Me.btnSair.TabIndex = 3
+        Me.btnSair.Text = "S&air"
+        Me.btnSair.UseVisualStyleBackColor = True
+        '
+        'btnAdiciona
+        '
+        Me.btnAdiciona.Location = New System.Drawing.Point(12, 208)
+        Me.btnAdiciona.Name = "btnAdiciona"
+        Me.btnAdiciona.Size = New System.Drawing.Size(95, 32)
+        Me.btnAdiciona.TabIndex = 4
+        Me.btnAdiciona.Text = "Adicionar 1"
+        Me.btnAdiciona.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 176)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 26)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Mostra Variáveis"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnElse
+        '
+        Me.btnElse.Location = New System.Drawing.Point(637, 321)
+        Me.btnElse.Name = "btnElse"
+        Me.btnElse.Size = New System.Drawing.Size(121, 26)
+        Me.btnElse.TabIndex = 6
+        Me.btnElse.Text = "Botão Condicional"
+        Me.btnElse.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(312, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(178, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Escreva um número p/comparar"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(364, 30)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(81, 23)
+        Me.txtNum1.TabIndex = 8
+        '
+        'btnComparar
+        '
+        Me.btnComparar.Location = New System.Drawing.Point(176, 27)
+        Me.btnComparar.Name = "btnComparar"
+        Me.btnComparar.Size = New System.Drawing.Size(121, 26)
+        Me.btnComparar.TabIndex = 9
+        Me.btnComparar.Text = "Botão Comparar"
+        Me.btnComparar.UseVisualStyleBackColor = True
+        '
+        'btnVerifica
+        '
+        Me.btnVerifica.Location = New System.Drawing.Point(499, 25)
+        Me.btnVerifica.Name = "btnVerifica"
+        Me.btnVerifica.Size = New System.Drawing.Size(121, 30)
+        Me.btnVerifica.TabIndex = 10
+        Me.btnVerifica.Text = "Verifica Número"
+        Me.btnVerifica.UseVisualStyleBackColor = True
+        '
+        'btnFor
+        '
+        Me.btnFor.Location = New System.Drawing.Point(642, 180)
+        Me.btnFor.Name = "btnFor"
+        Me.btnFor.Size = New System.Drawing.Size(117, 28)
+        Me.btnFor.TabIndex = 11
+        Me.btnFor.Text = "Laço Lista"
+        Me.btnFor.UseVisualStyleBackColor = True
+        '
+        'lstDados
+        '
+        Me.lstDados.FormattingEnabled = True
+        Me.lstDados.ItemHeight = 15
+        Me.lstDados.Location = New System.Drawing.Point(645, 73)
+        Me.lstDados.Name = "lstDados"
+        Me.lstDados.Size = New System.Drawing.Size(113, 94)
+        Me.lstDados.TabIndex = 12
+        '
+        'btnWhile
+        '
+        Me.btnWhile.Location = New System.Drawing.Point(642, 42)
+        Me.btnWhile.Name = "btnWhile"
+        Me.btnWhile.Size = New System.Drawing.Size(117, 25)
+        Me.btnWhile.TabIndex = 13
+        Me.btnWhile.Text = "Do While Lista"
+        Me.btnWhile.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnWhile)
+        Me.Controls.Add(Me.lstDados)
+        Me.Controls.Add(Me.btnFor)
+        Me.Controls.Add(Me.btnVerifica)
+        Me.Controls.Add(Me.btnComparar)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnElse)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnAdiciona)
+        Me.Controls.Add(Me.btnSair)
+        Me.Controls.Add(Me.btnNome)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Tag = ""
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnNome As Button
+    Friend WithEvents btnSair As Button
+    Friend WithEvents btnAdiciona As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnElse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents btnComparar As Button
+    Friend WithEvents btnVerifica As Button
+    Friend WithEvents btnFor As Button
+    Friend WithEvents lstBox As ListBox
+    Friend WithEvents lstDados As ListBox
+    Friend WithEvents btnWhile As Button
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnNome.Click
+        'Mostra uma caixa de mensagem para o usuário. 
+
+        MessageBox.Show("Olá, " & txtName.Text & "Bem Vindo ao Primeiro projeto de Windows Forms da Mari.", "Mensagem ao usuário.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnSair.Click
+        'Metodo que finaliza o formulário/programa.
+        Me.Close()
+    End Sub
+
+    Private Sub btnAdiciona_Click(sender As Object, e As EventArgs) Handles btnAdiciona.Click
+        'Método criado para adicionar 1 a um valor padrão já existente em uma variável
+
+        Dim intNumero As Integer
+        intNumero = 27
+        intNumero = intNumero + 1
+        MessageBox.Show("Valor de Numérico + 1 = " & intNumero.ToString, "Variáveis")
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        'Método que mostra variáveis criadas em novas janelas.
+        Dim varInteira As Integer = 34   'Tipo de variável numérica que representa os pontos fixos.
+        Dim varDouble As Double = 24.5   'Tipo de variável numérica que representa os pontos flutuantes. 
+        Dim varString As String = "Olá Mundo."
+        Dim varBoolean As Boolean = True
+
+        MsgBox(varInteira)
+        MsgBox(varDouble)
+        MsgBox(varString)
+        MsgBox(varBoolean)
+
+        'Outra forma de exibir na tela além do MsgBox
+        MessageBox.Show(varString, "Essa variável é uma string.")
+        MessageBox.Show("Demonstrando a variável String" & varDouble.ToString, "É um ponto flutuante")
+    End Sub
+
+    Private Sub btnElse_Click(sender As Object, e As EventArgs) Handles btnElse.Click
+        Dim varTeste1 As Integer = 30
+
+        If varTeste1 < 20 Then
+            MessageBox.Show("Esse valor é MENOR que 200" & varTeste1, "FIM")
+        ElseIf varTeste1 = 34 Then
+            MessageBox.Show("Esse valor é IGUAL a 30" & varTeste1, "FIM")
+        Else
+            MessageBox.Show("Esse valor é MAIOR que 200" & varTeste1, "FIM")
+        End If
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles btnComparar.Click
+        'Método para verificar se as comparações estão sendo obedecidas. 
+
+        Dim string1 As String
+        string1 = txtName.Text
+        Dim numeroComp1 As Integer
+        numeroComp1 = txtNum1.Text
+
+        If string1 <> "Jose" And numeroComp1 <> -3 Then
+            MessageBox.Show(string1, "O nome não é José e o número é = ." & numeroComp1)
+        Else
+            MessageBox.Show(string1 & numeroComp1)
+        End If
+    End Sub
+
+    Private Sub btnVerifica_Click(sender As Object, e As EventArgs) Handles btnVerifica.Click
+        'Método criado para verificar se o valor escrito na escreva um número de comparação é REALMENTE um número inteiro. 
+        Dim novoInter1 As Integer
+        Try
+            novoInter1 = txtNum1.Text
+        Catch                    'Se existir uma falha nessa execução, ele entra no catch e não executa mais NADA.
+            Exit Sub
+        End Try
+
+        If novoInter1 > 0 Then
+            MessageBox.Show("O número é INTEIRO" & novoInter1)
+        Else
+            MessageBox.Show("O número NÃO é inteiro." & novoInter1)
+        End If
+    End Sub
+
+    Private Sub btnFor_Click(sender As Object, e As EventArgs) Handles btnFor.Click
+        ' Método criado para testar o FOR.
+        Dim intLoop As Integer
+        LimparLista()         'Referenciando o sub método de limpar lista criado 
+        For index = 1 To 5    'O for vai criar uma lista onde os itens criados serão adicionados a lista.
+            lstDados.Items.Add("Lista de Itens possui " & intLoop.ToString & " no total.")  'Imprimindo a quantidade de valores da lista
+        Next
+    End Sub
+    Private Sub LimparLista()
+        'Submétodo para limpar a lista
+        lstDados.Items.Clear()
+    End Sub
+
+    Private Sub btnWhile_Click(sender As Object, e As EventArgs) Handles btnWhile.Click
+        Dim intLoopWhile As Integer
+
+        'Chamada da sub rotina para limpar a lista.
+        LimparLista()
+
+        'Executa o loop na lista(semelhante ao FOR, mas não igual)
+        Do While intLoopWhile < 5
+
+            lstDados.Items.Add(intLoopWhile.ToString)
+            intLoopWhile += 1
+        Loop
+    End Sub
+End Class
